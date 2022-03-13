a77ca3f754b074d64d3ae20d02439a087198b2b6
Lev <noviykomp13@mail.ru>
2022-03-13 18:55:58 +0300

    small connection change

--- src/main/java/Connection/DB_connection.java
+++ src/main/java/Connection/DB_connection.java
@@ -4,18 +4,17 @@ import java.sql.*;
 
 public class DB_connection {
     public static Statement pass_statement;
+    private static final String URL = "jdbc:mysql://localhost:3306/university";
+    private static final String NAME = "root";
+    private static final String PASSWORD = "yjdtqibqjhajuhfabxtcrbqckjdfhm,jktt100000ckjd!";
 
     public static Statement Connect_to_database() throws SQLException {
-        final String url = "jdbc:mysql://localhost:3306/university";
-        final String uname = "root";
-        final String password = "yjdtqibqjhajuhfabxtcrbqckjdfhm,jktt100000ckjd!";
-
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-            Connection con = DriverManager.getConnection(url, uname, password);
+            Connection con = DriverManager.getConnection(URL, NAME, PASSWORD);
             Statement statement = con.createStatement(); // sout
 
         return pass_statement = statement;

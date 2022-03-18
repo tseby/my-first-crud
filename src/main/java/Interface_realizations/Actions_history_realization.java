c457a3fa57f84750b8e402cef0f8a7acc7a630cb
Lev <noviykomp13@mail.ru>
2022-03-18 18:04:45 +0300

    wip

--- src/main/java/Interface_realizations/Actions_history_realization.java
+++ src/main/java/Interface_realizations/Actions_history_realization.java
@@ -5,7 +5,7 @@ import Interfaces.Actions_history;
 import java.util.ArrayList;
 
 public class Actions_history_realization implements Actions_history {
-
+Admin_interface_realization admin_interface_realization = new Admin_interface_realization();
     public ArrayList<String> past_user_actions = new ArrayList<>();
 
 
@@ -14,6 +14,7 @@ public class Actions_history_realization implements Actions_history {
         for (int i = 0; i < past_user_actions.size();i++){
             System.out.println(past_user_actions.get(i));
         }
+        admin_interface_realization.Welcome_statment();
         return false;
     }
 
@@ -21,12 +22,14 @@ public class Actions_history_realization implements Actions_history {
     public boolean clean_history() {
         past_user_actions.clear();
         add_into_history("History of actions has been cleaned");
+        admin_interface_realization.Welcome_statment();
         return false;
     }
 
     @Override
     public boolean add_into_history(String action) {
         past_user_actions.add(action);
+        admin_interface_realization.Welcome_statment();
         return false;
     }
 }
